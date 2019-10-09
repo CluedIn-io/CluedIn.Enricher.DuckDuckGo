9e5bef0893912dd3ec74ea78ec3e945fa3bfadaa
Ben Fidge <ben.fidge@btopenworld.com>
2019-10-09 09:31:02 +0700

    Added integration tests

---
+++ test/integration/ExternalSearch.DuckDuckGo.Integration.Tests/DuckDuckGoTests.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DuckDuckGoTests.cs" company="Clued In">
+//   Copyright (c) 2018 Clued In. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the duck go tests class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Messages.Processing;
+using CluedIn.ExternalSearch.Providers.DuckDuckGo;
+using CluedIn.Testing.Base.ExternalSearch;
+using Moq;
+using Xunit;
+
+namespace ExternalSearch.DuckDuckGo.Integration.Tests
+{
+    public class DuckDuckGoTests : BaseExternalSearchTest<DuckDuckGoExternalSearchProvider>
+    {
+        [Theory(Skip = "Failed Mock exception. GitHub Issue 829 - ref https://github.com/CluedIn-io/CluedIn/issues/829")]
+        [InlineData("Sitecore", "https://www.sitecore.com")]
+        [InlineData("Microsoft", "https://www.microsoft.com")]
+        [InlineData("Nordea", null)]
+        [InlineData(null, "nordea.dk")]
+        [InlineData(null, "nordea.com")]
+        public void TestClueProduction([CanBeNull] string name, [CanBeNull] string website)
+        {
+            var properties = new EntityMetadataPart();
+
+            if (name != null)
+                properties.Properties.Add(CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInOrganization.OrganizationName, name);
+
+            if (website != null)
+                properties.Properties.Add(CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInOrganization.Website, website);
+
+            IEntityMetadata entityMetadata = new EntityMetadataPart() {
+                Name = name,
+                EntityType = EntityType.Organization,
+                Properties = properties.Properties
+            };
+
+            this.Setup(new object[0], entityMetadata);
+
+            this.testContext.ProcessingHub.Verify(h => h.SendCommand(It.IsAny<ProcessClueCommand>()), Times.AtLeastOnce);
+            Assert.True(this.clues.Count > 0);
+        }
+
+        [Theory]
+        [InlineData("Non-Existing Organization", "https://www.itsdummy.companysite/")]
+        [InlineData("Hanging gardens", null)]
+        [Trait("Category", "slow")]
+        public void TestNoClueProduction(string name, [CanBeNull] string website)
+        {
+            var properties = new EntityMetadataPart();
+            properties.Properties.Add(CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInOrganization.OrganizationName, name);
+
+            if (website != null)
+                properties.Properties.Add(CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInOrganization.Website, website);
+
+            IEntityMetadata entityMetadata = new EntityMetadataPart() {
+                Name = name,
+                EntityType = EntityType.Organization,
+                Properties = properties.Properties
+            };
+
+            this.Setup(new object[0], entityMetadata);
+
+            this.testContext.ProcessingHub.Verify(h => h.SendCommand(It.IsAny<ProcessClueCommand>()), Times.Never);
+            Assert.True(this.clues.Count == 0);
+        }
+    }
+}
+
---
+++ test/integration/ExternalSearch.DuckDuckGo.Integration.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ece68f65-8825-46f1-8d47-20f3173b27d6")]
