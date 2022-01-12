7d13cd9e2a7b7026c61e14e9356442b854188192
nicholasbarlow <nicholasbarlow1@gmail.com>
2022-01-12 16:48:14 +0000

    feature: Move onto the new provider model

---
+++ src/ExternalSearch.Providers.DuckDuckgo/DuckDuckGoExternalSearchJobData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CluedIn.Core.Crawling;
+
+namespace CluedIn.ExternalSearch.Providers.DuckDuckgo
+{
+    public class DuckDuckGoExternalSearchJobData : CrawlJobData
+    {
+        public DuckDuckGoExternalSearchJobData(IDictionary<string, object> configuration)
+        {
+        }
+
+        public IDictionary<string, object> ToDictionary()
+        {
+            return new Dictionary<string, object>();
+        }
+    }
+}
