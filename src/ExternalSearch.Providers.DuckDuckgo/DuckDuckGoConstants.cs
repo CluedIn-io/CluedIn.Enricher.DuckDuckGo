490e1af1aaafe7e9f73d1b64afed14ca3ed0862e
Yi-Wei <ywei390@gmail.com>
2024-11-19 09:35:46 +0800

    feat: Add help text to enricher configuration page

--- src/ExternalSearch.Providers.DuckDuckgo/DuckDuckGoConstants.cs
+++ src/ExternalSearch.Providers.DuckDuckgo/DuckDuckGoConstants.cs
@@ -25,28 +25,31 @@ namespace CluedIn.ExternalSearch.Providers.DuckDuckgo
 
         public static AuthMethods AuthMethods { get; set; } = new AuthMethods 
         { 
-            token = new List<Control>()
+            Token = new List<Control>()
             {
                 new Control()
                 {
-                    displayName = "Accepted Entity Type",
-                    type = "input",
-                    isRequired = false,
-                    name = KeyName.AcceptedEntityType
+                    DisplayName = "Accepted Entity Type",
+                    Type = "input",
+                    IsRequired = false,
+                    Name = KeyName.AcceptedEntityType,
+                    Help = "The entity type that defines the golden records you want to enrich (e.g., /Organization)."
                 },
                 new Control()
                 {
-                    displayName = "Organization Name vocab key",
-                    type = "input",
-                    isRequired = false,
-                    name = KeyName.OrgNameKey
+                    DisplayName = "Organization Name Vocabulary Key",
+                    Type = "input",
+                    IsRequired = false,
+                    Name = KeyName.OrgNameKey,
+                    Help = "The vocabulary key that contains the names of companies you want to enrich (e.g., organization.name)."
                 },
                 new Control()
                 {
-                    displayName = "Website vocab key",
-                    type = "input",
-                    isRequired = false,
-                    name = KeyName.WebsiteKey
+                    DisplayName = "Website Vocabulary Key",
+                    Type = "input",
+                    IsRequired = false,
+                    Name = KeyName.WebsiteKey,
+                    Help = "The vocabulary key that contains the websites of companies you want to enrich (e.g., organization.website)."
                 },
             }
         };
